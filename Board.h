6a5423d55e7e16668245e78343d84f317afcf636
Madeline Coco <madelinecoco@Madelines-MacBook-Pro.local>
2023-11-28 13:09:08 -0500

    add board class impl files

--- Board.h
+++ Board.h
@@ -1,2 +1,30 @@
 // Project 4 - Sudoku (EECE2560)
+// Part a
 // Collaborators: Madeline Coco, Neha Joby
+// This file contains the definitions for the board class
+
+#ifndef BOARD_H
+#define BOARD_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Board 
+{
+    public:
+    //Default constructor, initialize board
+    Board();
+    // prints the board and conflict vectors to the screen
+    void printBoard();
+    // add a value to a cell, update conflict vectors
+    void addValue();
+
+    private:
+    vector<vector<int>> _board;
+    vector<vector<bool>> rowConflicts;
+    vector<vector<bool>> colConflicts;
+    vector<vector<bool>> squareConflicts;
+};
+
+#endif
---
+++ board.cpp
@@ -0,0 +1,18 @@
+// Implementation file for board class
+
+#include "board.h"
+
+Board::Board()
+{
+    //add constructor impl
+}
+
+void Board::printBoard()
+{
+    //add print function impl
+}
+
+void Board::addValue()
+{
+    //add print function impl
+}
