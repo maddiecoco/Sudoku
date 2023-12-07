c34f1fcc3ab9b541423ca139cb65fde101ee513f
Neha Joby <106851456+nehajoby@users.noreply.github.com>
2023-12-07 13:16:55 -0800

    Fully commented board.h

--- part_b/board.h
+++ part_b/board.h
@@ -1,12 +1,11 @@
-//
-// Created by nehae on 12/4/2023.
-//
+// Maddie & Neha
+// Sudoku Puzzle Part B
 
 
 #ifndef BOARD_H
 #define BOARD_H
 
-
+// Standard libraries and header files used.
 #include <iostream>
 #include <limits.h>
 #include "d_matrix.h"
@@ -14,61 +13,59 @@
 #include <list>
 #include <fstream>
 
-using namespace std;
+using namespace std;    // Makes standard library names available.
 
 typedef int ValueType; // The type of the value in a cell
 const int blank = -1;  // Indicates that a cell is blank
 
-const int squareSize = 3;  //  The number of cells in a small square
-//  (usually 3).  The board has
-//  SquareSize^2 rows and SquareSize^2
-//  columns.
-
+// Constants that define dimensions and value limits for the board.
+const int squareSize = 3;   //  The number of cells in a small square
+                            //  (usually 3).  The board has
+                            //  SquareSize^2 rows and SquareSize^2
+                            //  columns.
 const int boardSize = squareSize * squareSize;
-
 const int minValue = 1;
 const int maxValue = 9;
 
-//int numSolutions = 0;
 
+// Board class declaration - Stores the entire Sudoku board.
 class Board
-    // Stores the entire Sudoku board
 {
 public:
-    Board(int sqSize); //Constructor
-    void Clear(); //Clears the board
-    void Initialize(ifstream &fin); //Initalizes the board with ifstream
-    bool IsSolved(); //Returns if the board is solved
-    void Print(); //Prints out the board
-    void PrintConflicts(); //Prints out the conflicts
+    Board(int sqSize);                 // Constructor - Initializes a Sudoku board of given size.
+    void Clear();                      // Clears the board
+    void Initialize(ifstream &fin);    // Initalizes the board with ifstream
+    bool IsSolved();                   // Returns if the board is solved
+    void Print();                      // Prints out the board
+    void PrintConflicts();             // Prints out the conflicts
 
-    bool IsBlank(int i, int j); //Returns if cell is blank
-    ValueType GetCell(int i, int j); //Returns value of cell
-    void SetCell(int i, int j, ValueType val); //Sets the cell to val
-    void ClearCell(int i, int j); //Clears the cell
+    bool IsBlank(int i, int j);                    // Returns if cell is blank
+    ValueType GetCell(int i, int j);               // Returns value of cell
+    void SetCell(int i, int j, ValueType val);     // Sets the cell to val
+    void ClearCell(int i, int j);                  // Clears the cell
 
-    bool CausesConflict(int i, int j, ValueType val); //Would it cause conflict
-    void AddConflict(int i, int j, ValueType val); //Add a conflict
-    void RemoveConflict(int i, int j, ValueType val); //Remove conflict
+    bool CausesConflict(int i, int j, ValueType val);  // Would it cause conflict
+    void AddConflict(int i, int j, ValueType val);     // Add a conflict
+    void RemoveConflict(int i, int j, ValueType val);  // Remove conflict
 
-    // added in part 4b
-      bool isLegal(int, int, int);
-      bool nextCell(int &, int &);
-      bool solve(int &count);
-      int getRecursions();
-      int recursions;
+    // Part B additions - Sudoku solving methods
+    bool isLegal(int, int, int);    // Checks if a move is legal
+    bool nextCell(int &, int &);    // Finds the next cell to be filled
+    bool solve(int &count);         // Solves the Sudoku puzzle recursively
+    int getRecursions();            // Returns the number of recursive calls made
+    int recursions;                 // An integer variable to track recursion count
 
 private:
-
-    // The following matrices go from 1 to BoardSize in each
-    // dimension.  I.e. they are each (BoardSize+1) X (BoardSize+1)
-    matrix<ValueType> value;
+// The following matrices go from 1 to BoardSize in each dimension.
+// they are each (BoardSize+1) X (BoardSize+1)
+    matrix<ValueType> value;        // Matrix to store cell values
+    
+    // Track conflicts in rows, columns, and squares
     matrix<bool> rowConflicts;
     matrix<bool> colConflicts;
     matrix<bool> sqConflicts;
-    // Keeps track of the digits that have been placed
-    matrix<vector<bool>> placed;
-
+    
+    matrix<vector<bool>> placed;    // Tracks digits that have been placed on the board
 };
 
 
